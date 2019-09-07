9e3442d402017b941fec4769ddc42cd4a5574f54
MajorSaheb <aashutoshprakash94@gmail.com>
2019-09-07 20:27:09 +0530

    css changes

--- src/Components/loginPage/index.js
+++ src/Components/loginPage/index.js
@@ -3,14 +3,15 @@ import './loginPage.css';
 
 
 function LoginPage(props) {
-  const [name, setName] = useState('John Doe');
-  const [password, setPassword] = useState('123')
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('')
   const handleNameInput = e => {
     e.target.type==="text" ? setName(e.target.value) : setPassword(e.target.value);
   };
     return (
       <div className="loginPage">
         <div className="content">
+          <h3>Login</h3>
           <label>Email: </label>
           <input type='text' placeholder='Email' onChange={handleNameInput} value={name} required/>
           <label>Password: </label>
